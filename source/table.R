d83454ffdeb67cd6daa15252a9e65db61859c748
rusheelc04 <rusheelchande04@gmail.com>
2022-11-16 21:05:23 -0800

    committing all directory changes

--- source/table.R
+++ source/table.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-counties<- read.csv("C:\\Users\\Katie\\Documents\\info201\\Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2021_2.csv")
+counties<- read.csv("../data/Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2021.xlsx - in.csv")
 View(counties)
 
 counties<- counties %>%
