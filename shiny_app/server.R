d7246fd276cc6efa7226bf7459d8999de98c2834
katie1616 <khulst@uw.edu>
2022-12-05 09:49:25 -0800

    updated

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -5,14 +5,7 @@ library(gsheet)
 library(dplyr)
 library(ggplot2)
 
-
 crime_df <- read_csv("C:\\Users\\Katie\\Documents\\info201\\assignments\\project-group-1-section-ae\\data\\Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2021.xlsx - in.csv")
-##violent_crimes<- c(crime_df$Murder,	crime_df$Rape, crime_df$Robbery, crime_df$Aggravated_assault)
-##property_crime<- c(crime_df$Burglary, crime_df$Larceny-theft,	crime_df$vehicle_theft, crime_df$Arson)
-state_property_crime <- crime_df %>% 
-  group_by(state_abbreviation) %>% summarise(Property_crime = sum(Property_crime)) %>% select(state_abbreviation, Property_crime)
-
-crime_df <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1DvqRh0LNJIhLDdJyG5-ZQlFeGucNzO3b/edit#gid=557182089")
 state_violent_crime <- crime_df %>% 
   group_by(state_abbreviation) %>% 
   summarize(Violent_crime = sum(Violent_crime, na.rm = TRUE)) %>% 
@@ -23,27 +16,22 @@ state_violent_crime <- crime_df %>%
 function(input, output) {
   
   # Fill in the spot we created for a plot
-  output$crime_plot <- renderPlot({
+  output$crime_bar_plot <- renderPlot({
     
-      #barplot(crime_df[,input$State], 
-              #main=input$State,
-              #ylab="Number of Telephones",
-              #xlab="Year")
-    
-    plot_bar_crime <- crime_df %>%
-      ggplot(aes(y=Violent_crime, x= input$State))+
-      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))+
-      geom_col()+
-      scale_x_discrete(guide = guide_axis(n.dodge= 2))+
-      labs(title="Violent Crime by State", x = "States", y = "Violent Crime")
-    
+     plot_bar_crime <- crime_df %>%
+       ggplot(aes(y=Violent_crime, x= input$State))+
+       
+       theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))+
+       scale_x_discrete(guide = guide_axis(n.dodge= 2))+
+       labs(title="Violent Crime by State", x = "States", y = "Violent Crime")
+     plot_bar_crime
       })
 }
   server <- function(input, output) {
     
-    output$plot_crime <- renderPlot({
+    output$line_crime <- renderPlot({
       state_violent_crime %>%
-        ggplot(aes(y=input$Violent_crime, x= state_abbreviation))+
+        ggplot(aes(y=Violent_crime, x= state_abbreviation))+
         theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))+
         geom_line(aes(group = 1))+
         geom_point()+
@@ -51,5 +39,16 @@ function(input, output) {
         labs(title="Violent Crime by State", x = "States", y = "Violent Crime")
       
       
+    })
+    output$crime_bar_plot <- renderPlot({
+      # data wrangle crime_df
+      
+      plot_bar_crime <- crime_df %>%
+        ggplot(aes(y=Violent_crime, x= input$State))+
+        geom_col() +
+        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))+
+        scale_x_discrete(guide = guide_axis(n.dodge= 2))+
+        labs(title="Violent Crime by State", x = "States", y = "Violent Crime")
+      plot_bar_crime
     })
   }
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -2,7 +2,6 @@ library("shiny")
 library(plotly)
 library(tidyverse)
 library("shiny")
-library(ggplot2)
 library(shiny)
 library(gsheet)
 library(dplyr)
@@ -20,20 +19,21 @@ fluidPage(
     "Introduction", # label for the tab in the navbar
     titlePanel("Introduction"), # show with a displayed title
       mainPanel(
-        h3("Primary Content"),
-        p("Insert Introduction here:")
+        h3("Summary:"),
+        strong("Daisy Shiewoo Lee, Katie Hulst, Rusheel Chande, Yulia Danielyan"),
+        p("Our project will be a website that shows many different crimes which you will be able to filter to the crime that you want to see. You can filter out the city you would like to see and view how widely involved this crime is in that area. The user will also be able to control what date they would like to see the data being in, the earliest being the year of 2021 to give a more current timeline. They can filter the distance within the city they are looking at to show how zoomed in or zoomed out they want to see the data. We will be using variations of a color shade to show areas that have a hotter trend of crime and others that lay low with crime. There will be tabs to give other information about the project or information about crime.")
       )
     ),
     
     # Define content for the second page
     page_two <- tabPanel(
-      "Map", # label for the tab in the navbar
-        titlePanel("Map"), # show with a displayed title
+      "Scatter Plot", # label for the tab in the navbar
+        titlePanel("Scatter Plot"), # show with a displayed title
         # This content uses a sidebar layout
         sidebarLayout(
           sidebarPanel(
             h3("Summary"),
-            p("Insert summary paragraph here:")
+            p("This ")
           ),
           mainPanel(
             h3("Primary Content"),
@@ -57,10 +57,12 @@ fluidPage(
             ),
             
             h3("Summary:"),
-            p("Insert summary paragraph here:")
+            p("This chart shows the violent crime by population. It highlights 
+              certain states rates compared to others and allows the user to see the states
+               with the highest and lowest amounts of crime. This shows that states with bigger populations and cities have more crime.")
           ),
           mainPanel(
-            plotOutput("plot_crime")
+            plotOutput("line_crime")
           )
         )
     ),
@@ -75,17 +77,15 @@ fluidPage(
         sidebarLayout(
           sidebarPanel(
             
-            sidebarPanel(
               selectInput("State", "State", 
                           choices=crime_df$State),
               ##hr(),
               ##helpText(""),
               h3("Summary: "),
-              p("Plots, data tables, etc. would go here")
-          )
+              p("This chart focuses in on certain states and allows for the amount of violent crime within a state to seen more clearly. It allows for the trends within the data to be more clearly analyzed. ")
           ),
           mainPanel(
-            plotOutput("plot_bar_crime")
+            plotOutput("crime_bar_plot")
           #)
         )
       )
@@ -106,7 +106,11 @@ fluidPage(
       "Report", # label for the tab in the navbar
       mainPanel(
         h3("Report: "),
-        p("Insert Report Here:")
+        p("Our findings provide crucial information regarding crime. We analyzed crime all over the United States with a focus on metropolitan counties, what we counted as “cities” and non-metropolitan counties. We analyzed violent crimes, including homicide, rape, robbery, and aggravated assault, and property crimes, which includes burglary, Larceny-theft, motor chicle theft and arson.
+
+It's no secret that crime is everywhere. While we like to believe we would never experience this crime first hand, that is not always the case. Our friends, neighbors and strangers are victims of these crimes. Crime occurs all around our country, in many different forms. Our work provides a reminder that crime happens all around us. It provides a reminder to be vigilant and stay safe. To be aware of your surroundings and be cautious. While we focused on metropolitan areas, where most people expect crime to occur, non-metropolitan areas, where most people expect no crime to occur, also experienced similar trends with the data. While the numbers in the non-metropolitan areas are small, they still exist, and are quite shocking when expecting these numbers to be non-existent, showing the cracks within our society and issues with our policing system. Many other countries have tried different approaches to policing and have been relatively successful. The United States has not tried any new approaches and it’s evident, through our data, that the current system does not work. We need to do something to fix the number of crimes we see in the United States. The number of violent crimes within a county might seem low, but we must remember the context. Within our data there are over two thousand counties included meaning the amount of crimes we see within a year is extremely large. It is also important to note that our data is limited in California, Florida, Illinois, Maryland, New Jersey, New Mexico, New York, and Pennsylvania. This means that states with some of the largest populations are not entirely included due to issues with their police stations reporting procedures. With this context, the numbers are still very large and show that reform is needed.
+
+The United States of America needs to work harder to stop crime. While we understand it would be a bit naive to hope that our findings will cause change, at the very least we hope our findings will provide more evidence for the fight for reform, in the ongoing debate on police reform.")
         )
     )
 )
